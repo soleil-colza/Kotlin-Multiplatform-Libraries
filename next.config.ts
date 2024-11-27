0781791c34363035cae45c1f22fde8267ad23818
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-11-28 06:59:46 +0900

    chore: Configure Next.js with basePath and export output option

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const repoName = process.env.REPO_NAME;
+const basePath = repoName ? `/${repoName}` : undefined;
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath,
 };
 
 export default nextConfig;
